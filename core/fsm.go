144665eb205ddd6cfdaec84abe999098c515033f
gtygo <gtydoit@gmail.com>
2019-11-18 17:32:15 +0800

    refactor code structure of package core

---
+++ core/fsm.go
@@ -0,0 +1,42 @@
+package core
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/gtygo/Ourea/raft"
+	hraft "github.com/hashicorp/raft"
+)
+
+type fsm struct {
+	db     DB
+	logger *log.Logger
+}
+
+func NewFsm(path string) (*fsm, error) {
+	db, err := NewBadgerDB(path, path)
+	if err != nil {
+		return nil, err
+	}
+	return &fsm{
+		db:     db,
+		logger: log.New(os.Stderr, "[fsm] ", log.LstdFlags),
+	}, nil
+}
+
+func (f *fsm) Get(key string) (string, error) {
+	return "", nil
+}
+
+func (f *fsm) Apply(l *raft.Log) interface{} {
+	return nil
+}
+
+func (f *fsm) SnapShot() (hraft.FSMSnapshot, error) {
+	return nil, nil
+}
+
+func (f *fsm)Restore(readClose io.ReadCloser)error{
+	return nil
+}
---
+++ core/store.go
@@ -0,0 +1,60 @@
+package core
+
+import (
+	"github.com/hashicorp/raft"
+	"log"
+	"os"
+)
+
+type Store struct {
+	RaftDir  string
+	RaftBind string
+
+	raft *raft.Raft
+
+	// Generate snapshot, restore snapshot.
+	fsm *fsm
+
+	logger *log.Logger
+}
+
+func NewStore(raftDir, raftBind string) (*Store, error) {
+	fsm, err := NewFsm(raftDir)
+	if err != nil {
+		return nil, err
+	}
+	return &Store{
+		RaftDir:  raftDir,
+		RaftBind: raftBind,
+		fsm:      fsm,
+		logger:   log.New(os.Stderr, "[store] ", log.LstdFlags),
+	}, nil
+}
+
+func (s *Store) Open(isStrap bool, number string) error {
+	return nil
+}
+
+func (s *Store) Get(key string) (string, error) {
+	return "s.fsm.Get(key)", nil
+}
+
+func (s *Store) Set(key, value string) error {
+	return nil
+}
+
+func (s *Store) Delete(key string) error {
+	return nil
+}
+
+func (s *Store) Join(id, addr string) error {
+	return nil
+}
+
+func (s *Store) Leave(id string) error {
+	return nil
+}
+
+func (s *Store) SnapShot() error {
+	return nil
+}
