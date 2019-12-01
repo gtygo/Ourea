cd945a7f23d6db9ed26fbcafa928ce9a7fe2f383
gtygo <gtydoit@gmail.com>
2019-12-01 22:32:07 +0800

    impl get & set

--- core/fsm.go
+++ core/fsm.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/gtygo/Ourea/raft"
 	hraft "github.com/hashicorp/raft"
 )
 
@@ -14,6 +13,9 @@ type fsm struct {
 	logger *log.Logger
 }
 
+
+
+//NewFsm return  a fsm instance
 func NewFsm(path string) (*fsm, error) {
 	db, err := NewDB(path, path)
 	if err != nil {
@@ -25,12 +27,31 @@ func NewFsm(path string) (*fsm, error) {
 	}, nil
 }
 
+//Get return value
 func (f *fsm) Get(key string) (string, error) {
-	return "", nil
+	f.logger.Printf("get key: %s from fsm store",key)
+	v, err := f.db.Get([]byte(key))
+	if err != nil {
+		return "", err
+	}
+	return string(v), nil
 }
 
-func (f *fsm) Apply(l *raft.Log) interface{} {
+func (f *fsm)Set(key ,value string)error{
+	f.logger.Printf("set key: %s vale: %s from fsm store",key,value)
+	err:=f.db.Set([]byte(key),[]byte(value))
+	if err!=nil{
+		return err
+	}
 	return nil
+}
+
+func (f *fsm) Apply(*hraft.Log) interface{} {
+	panic("implement me")
+}
+
+func (f *fsm) Snapshot() (hraft.FSMSnapshot, error) {
+	panic("implement me")
 }
 
 func (f *fsm) SnapShot() (hraft.FSMSnapshot, error) {
--- core/store.go
+++ core/store.go
@@ -1,9 +1,13 @@
 package core
 
 import (
+	boltraft "github.com/gtygo/Ourea/raft"
 	"github.com/hashicorp/raft"
 	"log"
+	"net"
 	"os"
+	"path/filepath"
+	"time"
 )
 
 type Store struct {
@@ -31,16 +35,60 @@ func NewStore(raftDir, raftBind string) (*Store, error) {
 	}, nil
 }
 
-func (s *Store) Open(isStrap bool, number string) error {
+func (s *Store) Open(isStrap bool, localID string) error {
+	conf := raft.DefaultConfig()
+	conf.LocalID = raft.ServerID(localID)
+	conf.SnapshotThreshold = 1024
+	addr, err := net.ResolveTCPAddr("tcp", s.RaftBind)
+	if err != nil {
+		s.logger.Println("kmbga")
+		return err
+	}
+	transport, err := raft.NewTCPTransport(s.RaftBind, addr, 3, 10*time.Second, os.Stderr)
+	if err != nil {
+		s.logger.Println("563hgd")
+		return err
+	}
+
+	ss, err := raft.NewFileSnapshotStore(s.RaftDir, 2, os.Stderr)
+	if err != nil {
+		s.logger.Println("9u8y7")
+		return err
+	}
+
+	boltDB, err := boltraft.NewStore(filepath.Join(s.RaftDir, "raft.db"))
+	if err != nil {
+		s.logger.Println("43234")
+		return err
+	}
+
+	r, err := raft.NewRaft(conf, s.fsm, boltDB, boltDB, ss, transport)
+	if err != nil {
+		s.logger.Println("12 ")
+		return err
+	}
+	s.raft = r
+	if isStrap {
+		config := raft.Configuration{
+			Servers: []raft.Server{
+				{
+					ID:      conf.LocalID,
+					Address: transport.LocalAddr(),
+				},
+			},
+		}
+		s.raft.BootstrapCluster(config)
+	}
+
 	return nil
 }
 
 func (s *Store) Get(key string) (string, error) {
-	return "s.fsm.Get(key)", nil
+	return s.fsm.Get(key)
 }
 
 func (s *Store) Set(key, value string) error {
-	return nil
+	return s.fsm.Set(key, value)
 }
 
 func (s *Store) Delete(key string) error {
